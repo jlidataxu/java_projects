cf60d0212e60fb86e58499842e799f87bac6a1c4
James Li <jli@dataxu.com>
2015-12-07 21:44:14 -0500

    tankwar0.2: add window close function, set size fixed, and add title

--- tank-war/src/TankClient.java
+++ tank-war/src/TankClient.java
@@ -1,4 +1,6 @@
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
 public class TankClient extends Frame{
@@ -6,6 +8,16 @@ public class TankClient extends Frame{
 	public void launchFrame() {
 		this.setLocation(400, 300);
 		this.setSize(800, 600);
+		this.setTitle("Tank War");
+		this.addWindowListener(new WindowAdapter(){
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			
+		});
+		this.setResizable(false);
 		setVisible(true);
 	}
 	public static void main(String[] args) {
