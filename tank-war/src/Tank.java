bb89a75af51dbce813bd05235692925c4de9f198
James Li <jli@dataxu.com>
2015-12-17 20:24:27 -0500

    tankwar1.2: 1) use case statement and drawLine to draw pt 2) update pt direction

--- tank-war/src/Tank.java
+++ tank-war/src/Tank.java
@@ -2,29 +2,101 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
-
+/* Tank class 
+ * 1.draw tank objects: cirle and pipe
+ * 2.movement: direction and speed
+ * 3.event: use key to control direction and fire missle
+ */
 public class Tank {
+	/* member variables
+	 * 1. Speed: XSPEED, YSPEED
+	 * 2. Size: WIDTH, HEIGHT
+	 * 3. Position: int x,y
+	 * 4. Associated TankClient
+	 * 5. Direction: 
+	 * 		boolean values, 
+	 * 		enum Direction, 
+	 * 		ptDirection, 
+	 * 		default tank direction
+	 * 		default ptDirection
+	 */
+	//1. SPEED
 	private static final int XSPEED = 5;
 	private static final int YSPEED = 5;
+	
+	//2. Size and 3.Location
 	public static final int WIDTH = 30;
 	public static final int HEIGHT = 30;
 	private int x;
 	private int y;
+	
+	//4. Associated TankClient Object
 	TankClient tc;
 	
+	//5. Directions
+	//5.1 boolean values which can associate with arrow keys
 	private boolean bU = false, bD = false, bL = false, bR = false;
+	//5.2 tank direction
 	enum Direction {R, RU, RD, L, LU, LD, U, D, STOP}
 	private Direction dir = Direction.STOP;
+	Direction ptDir = Direction.R;
 	
+	// Constructor
 	public Tank(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	//Overloaded Constructor 
 	public Tank(int x, int y, TankClient tc) {
 		this(x,y);
 		this.tc = tc;
 	}
+	/*
+	 *  Draw Method:
+	 *  1. Draw circle
+	 *  2. Draw pt
+	 *  3. run move() method to update location of tank object
+	 */
+	public void draw(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(x, y, WIDTH, HEIGHT);
+		g.setColor(c);
+		switch (ptDir) {
+		case L:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x, y+ Tank.HEIGHT/2);
+			break;
+		case LU:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x, y);
+			break;
+		case LD:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x, y+ Tank.HEIGHT);
+			break;
+		case R:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x + Tank.WIDTH, y+ Tank.HEIGHT/2);
+			break;
+		case RU:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x + Tank.WIDTH, y);
+			break;
+		case RD:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2,  x + Tank.WIDTH , y+ Tank.HEIGHT);
+			break;
+		case U:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x + Tank.WIDTH/2, y);
+			break;
+		case D:
+			g.drawLine(x + Tank.WIDTH/2, y + Tank.HEIGHT/2, x + Tank.WIDTH/2, y+ Tank.HEIGHT);
+			break;
+		}
+		move();
+	}
+	
+	/*
+	 *  move() method: 
+	 *  	1. use x,y variables to control and update location of tank
+	 *  	2. update pt direction
+	 */
 	
 	void move() {
 		switch (dir) {
@@ -59,16 +131,22 @@ public class Tank {
 		case STOP:
 			break;
 		}
+		
+		if(this.dir != Direction.STOP) {
+			this.ptDir = this.dir;
+		}
 	}
 	
-	public void draw(Graphics g) {
-		Color c = g.getColor();
-		g.setColor(Color.RED);
-		g.fillOval(x, y, WIDTH, HEIGHT);
-		g.setColor(c);
-		move();
-	}
 	
+	/* Event and Action
+	 * keyPressed: 
+	 * 		1.set corresponding boolean value to true
+	 * 		2.update tank direction
+	 * keyReleased: 
+	 * 		1. set corresponding boolean value to false
+	 * 		2. run fire() method
+	 * 		3. update tank direction
+	 */
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		int key = e.getKeyCode();
@@ -114,15 +192,16 @@ public class Tank {
 		
 		locateDirection();
 	}
-	
+	// fire method to generate missile object
 	public Missile fire() {
 		int x = this.x + Tank.WIDTH/2 - Missile.WIDTH/2;
 		int y = this.y + Tank.HEIGHT/2 - Missile.HEIGHT/2;
 		
-		Missile m = new Missile(x ,y,dir);
+		Missile m = new Missile(x ,y,ptDir);
 		return m;
 	}
-
+	
+	// locationDirection() method to update Direction
 	public void locateDirection() {
 		if (bU && !bD && !bL && !bR) {
 			dir = Direction.U;
